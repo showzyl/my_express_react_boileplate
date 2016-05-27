b9208085b9f13b083b7865f1aa1258a5dc806bfe
showzyl <showzyl520@gmail.com>
2016-05-27 15:28:06 +0800

    ci

--- client/page1/index.js
+++ client/page1/index.js
@@ -1,12 +1,24 @@
-require("./index.scss");
-require("../common/common.js");
+'use strict';
 
-console.log("client/page1/index.js");
 
-var drawEl = document.getElementById("draw");
-drawEl.innerHTML = "peko";
+import React from 'react';
+import { render } from 'react-dom';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
+
+import './index.scss';
+
+
+render(
+  <div style={{'marginTop': '20px'}}>
+    <DatePicker />
+    <div className="lizi">日历title</div>
+  </div>,
+  document.querySelector('#wrapper')
+);
+
 
 // Uncomment these to enable hot module reload for this entry.
-// if (module.hot) {
-//   module.hot.accept();
-// }
+ if (module.hot) {
+   module.hot.accept();
+ }
--- server/routes/page1.js
+++ server/routes/page1.js
@@ -1,8 +1,10 @@
 var express = require('express'),
-    router = express.Router();
+  router = express.Router();
 
-router.get('/', function(req, res) {
-    res.render('page1');
+router.get('/', function (req, res) {
+  res.render('page1', {
+    title: 'page1 test'
+  });
 });
 
 module.exports = router;
